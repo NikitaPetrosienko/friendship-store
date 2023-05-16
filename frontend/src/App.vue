6beea99fb881e7a4d49c64170b1346a56c873ea3
Gerasichev <gerasichev.serg@gmail.com>
2023-05-16 13:02:24 +0300

    Add axios

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,6 +4,17 @@
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import axios from 'axios';
+
+const getAlbums = async () => {
+  const response = await axios.get('http://127.0.0.1:8000/api/v1/albums/');
+  const data = await response.data;
+  console.log('data: ', data);
+}
+
+getAlbums();
+
+
 </script>
 
 <style lang="scss">
