195bd375f90cd0a10589ea68814ef5a03ce2e647
Gerasichev <gerasichev.serg@gmail.com>
2023-05-21 12:55:09 +0300

    Create Radiobutton component

---
+++ frontend/src/components/v-radiobutton/v-radiobutton.vue
@@ -0,0 +1,98 @@
+<script setup lang="ts">
+const emits = defineEmits(['update:checkedValue'])
+const props = defineProps({
+  name: {
+    type: String,
+    default: ''
+  },
+  id: {
+    type: String,
+    default: ''
+  },
+  value: {
+    type: String,
+    default: ''
+  },
+  label: {
+    type: String,
+    default: ''
+  },
+  checked: {
+    type: Boolean,
+    default: false
+  },
+  disabled: {
+    type: Boolean,
+    default: false
+  },
+  group: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const handleClick = (event) => {
+  emits('update:checkedValue', event.target.value)
+}
+</script>
+
+<template>
+  <input class="radiobutton" type="radio" :name="name" :id="id" :value="value" :checked="checked" :disabled="disabled"
+    @input="handleClick($event)">
+  <label :for="id">{{ label }}</label>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_variables.scss';
+@import '@/assets/scss/_mixins.scss';
+.radiobutton {
+  position: absolute;
+  z-index: -1;
+  opacity: 0;
+}
+.radiobutton + label {
+  display: inline-flex;
+  align-items: center;
+  user-select: none;
+}
+.radiobutton + label::before {
+  content: '';
+  display: inline-block;
+  width: 24px;
+  height: 24px;
+  flex-shrink: 0;
+  flex-grow: 0;
+  border: 1px solid #adb5bd;
+  border-radius: 6px;
+  margin-right: 10px;
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: 50% 50%;
+  border-radius: 50%;
+}
+
+.radiobutton:checked + label::before {
+  border-color: $blue;
+  background-color: $blue;
+  background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3cpath fill='%23fff' d='M6.564.75l-3.59 3.612-1.538-1.55L0 4.26 2.974 7.25 8 2.193z'/%3e%3c/svg%3e");
+}
+.radiobutton:not(:disabled):not(:checked) + label:hover::before {
+  border-color: $blue;
+}
+.radiobutton:not(:disabled):active + label::before {
+  background-color: $blue;
+  border: 1px solid #ECEBED;
+}
+.radiobutton:focus + label::before {
+  box-shadow: 0px 7px 20px rgba(0, 0, 0, 0.07);
+}
+
+.radiobutton:focus:not(:checked) + label::before {
+  border-color: $blue;
+}
+.radiobutton:disabled + label::before {
+  background-color: $gray;
+  border: 1px solid $gray;
+  background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3cpath fill='%23fff' d='M6.564.75l-3.59 3.612-1.538-1.55L0 4.26 2.974 7.25 8 2.193z'/%3e%3c/svg%3e");
+}
+</style>
