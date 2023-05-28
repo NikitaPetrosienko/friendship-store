bc625b6ee683481c6fef6a1fde33f48e51eb0f78
curtcobain2003 <petrosienko.nikita@mail.ru>
2023-05-28 13:53:55 +0300

    Create Products page

--- frontend/src/components/home-page/HomePageProducts.vue
+++ frontend/src/components/home-page/HomePageProducts.vue
@@ -1,27 +1,42 @@
 <script setup lang="ts">
+import AppProductsForm from '@/components/home-page/product/AppProdutctsForm.vue';
+import AppProduct from '@/components/home-page/product/AppProduct.vue';
+
 import { ref } from 'vue';
 import { useRoute } from 'vue-router';
 
-const brands = ref([
-  { id: 1, imagePath: "src/assets/img/brands/img1.png" },
-  { id: 2, imagePath: "src/assets/img/brands/img2.png" },
-  { id: 3, imagePath: "src/assets/img/brands/img3.png" },
+import { useBrandsStore } from '@/store/brands';
+
+// const route = useRoute();
+
+// const brandsStore = useBrandsStore();
+// brandsStore.fetchBrands();
+
+const products = ref([
+  { id: 1, imagePath: "/src/assets/img/brands/adidas/img1.jpeg", title: "Кеды Adidas Busenitz Vulc II Core Black FTWWHT VIVRED", price: 6700 },
+  { id: 2, imagePath: "/src/assets/img/brands/adidas/img2.jpeg", title: "Толстовка Adidas ELEVATED3HOODIE Collegiate Navy", price: 7150 },
+  { id: 3, imagePath: "/src/assets/img/brands/adidas/img3.png", title: "Толстовка adidas LIGHTWZIPTRACK Night Indigo Collegiate Green White", price: 5990 },
+  { id: 4, imagePath: "/src/assets/img/brands/adidas/img4.png", title: "Кеды adidas 3MC COLLEGIATE NAVY FTWWHT GRETWO", price: 4490 },
+  { id: 5, imagePath: "/src/assets/img/brands/adidas/img1.jpeg", title: "Кеды Adidas Busenitz Vulc II Core Black FTWWHT VIVRED", price: 6700 },
+  { id: 6, imagePath: "/src/assets/img/brands/adidas/img2.jpeg", title: "Толстовка Adidas ELEVATED3HOODIE Collegiate Navy", price: 7150 },
+  { id: 7, imagePath: "/src/assets/img/brands/adidas/img3.png", title: "Толстовка adidas LIGHTWZIPTRACK Night Indigo Collegiate Green White", price: 5990 },
+  { id: 8, imagePath: "/src/assets/img/brands/adidas/img4.png", title: "Кеды adidas 3MC COLLEGIATE NAVY FTWWHT GRETWO", price: 4490 },
 ]);
 
-const route = useRoute();
-
-console.log('route: ', route.params);
- 
+// console.log('Brand: ', route.params.brand);
+// console.log('Category: ', route.query.category);
 </script>
 
 <template>
-  <div class="home-page-brands">
+  <div class="homepage-products">
     <div class="container">
-      <div class="home-page-brands__row">
-        <div class="home-page-brands__column" v-for="brand in brands" :key="brand.id">
-          <div class="home-page-brands__card">
-            <img class="home-page-brands__img" :src="brand.imagePath" alt="brand">
-          </div>
+      <div class="homepage-products__title">Adidas</div>
+      
+      <AppProductsForm />
+
+      <div class="homepage-products__row">
+        <div class="homepage-products__column" v-for="product in products" :key="product.id">
+          <AppProduct :item="product"/>
         </div>
       </div>
     </div>
@@ -31,34 +46,32 @@ console.log('route: ', route.params);
 <style lang="scss" scoped>
 @import '@/assets/scss/_variables.scss';
 @import '@/assets/scss/_mixins.scss';
-.home-page-brands {
+.homepage-products {
   padding-top: 40px;
 }
-.home-page-brands__row {
-  display: flex;
-  justify-content: flex-start;
-  flex-wrap: wrap;
-  margin: 0 -38px;
+
+.homepage-products__title {
+  @include font(48px, 400, 60px);
+  margin: 0 0 20px;
   @include for-size(tablet) {
-    margin: 0 -24px;
-  }
-}
-.home-page-brands__column {
-  width: calc(100% / 3);
-  padding: 0 38px;
-  @include for-size(tablet) {
-    width: 100%;
-    padding: 0 24px;
+    @include font(30px, 400, 1.2);
   }
 }
 
-.home-page-brands__column+.home-page-brands__column {
+.homepage-products__row {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin: 30px -15px 0;
+}
+.homepage-products__column {
+  width: calc(100% / 4);
+  padding: 15px;
   @include for-size(tablet) {
-    margin-top: 40px;
+    width: calc(100% / 2);
   }
-}
-.home-page-brands__card {
-}
-.home-page-brands__img {
+  @include for-size(mobile) {
+    width: 100%;
+  }
 }
 </style>
