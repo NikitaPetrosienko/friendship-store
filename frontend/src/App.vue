3d1c8a26db9210a9bed9adae8831668f1a2f2218
curtcobain2003 <petrosienko.nikita@mail.ru>
2023-05-28 15:10:26 +0300

    [WIP] Change App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,15 +4,15 @@
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
-import axios from 'axios';
+// import axios from 'axios';
 
-const getAlbums = async () => {
-  const response = await axios.get('http://127.0.0.1:8000/api/v1/albums/');
-  const data = await response.data;
-  console.log('data: ', data);
-}
+// const getAlbums = async () => {
+//   const response = await axios.get('http://127.0.0.1:8000/api/v1/albums/');
+//   const data = await response.data;
+//   console.log('data: ', data);
+// }
 
-getAlbums();
+// getAlbums();
 
 
 </script>
