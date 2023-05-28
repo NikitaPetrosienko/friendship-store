b8efd1471834d3305514163095266199a1c5fd42
curtcobain2003 <petrosienko.nikita@mail.ru>
2023-05-28 13:56:27 +0300

    Create AppNews

---
+++ frontend/src/components/home-page/news/AppNews.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+import vPicture from '@/components/picture/v-picture.vue';
+
+import { PropType } from 'vue';
+
+interface IPropProduct {
+  id: number,
+  imagePath: string,
+  title: string,
+}
+
+const props = defineProps({
+  item: {
+    type: Object as PropType<IPropProduct>,
+    required: true,
+  },
+});
+
+</script>
+
+<template>
+  <div class="news">
+    <div class="news__body">
+      <v-picture :class-container="'news__picture'" :class-image="'news__img'" :image-url="item.imagePath"
+        :alt-text="item.title" />
+    </div>
+
+    <div class="news__footer">
+      <router-link to="/" class="news__link">
+        <div class="news__title">17 Окт. 2021</div>
+        <div class="news__title">{{ item.title }}</div>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_variables.scss';
+@import '@/assets/scss/_mixins.scss';
+
+.news {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+  background-color: $blue;
+}
+.news__body {
+  flex: 1 1 auto;
+}
+.news__link {
+  display: inline-block;
+  vertical-align: top;
+  max-width: 100%;
+  @include font(24px, 400, 30px);
+  text-decoration: none;
+  width: 100%;
+  color: $white;
+  overflow-wrap: break-word;
+  text-decoration: none;
+  padding: 15px;
+  
+  @include for-size(tablet) {
+    @include font(16px, 400, 1.2);
+  }
+}
+
+.news__link::after {
+  content: '';
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+}
+</style>
