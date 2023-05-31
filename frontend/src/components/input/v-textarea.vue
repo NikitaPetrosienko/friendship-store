8085ce5d63a5974e883387997b44373049766389
Gerasichev <gerasichev.serg@gmail.com>
2023-05-31 13:30:51 +0300

    Change Textarea component

--- frontend/src/components/input/v-textarea.vue
+++ frontend/src/components/input/v-textarea.vue
@@ -68,11 +68,12 @@ const updateValue = (e: any) => {
 }
 
 .textarea-error {
-  background: $red;
+  @include font(16px, 600, 1.2);
+  position: absolute;
   margin-top: 4px;
   border-radius: 7px;
   font-size: 13px;
-  color: #fff;
+  color: $red;
   padding: 5px;
 }
 
