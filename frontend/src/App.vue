f0a33fd7429efe99331a0e9cd4988a95d3ea6fae
Gerasichev <gerasichev.serg@gmail.com>
2023-06-01 14:22:10 +0300

    Change App.vue token setup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,6 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 
-import { useAuthStore } from '@/store/auth/auth';
-
-const authStore = useAuthStore();
-
-const token = JSON.parse(localStorage.getItem("authToken"));
-
-if (token) {
-  authStore.setUserToken(token);
-}
 </script>
 
 <template>
