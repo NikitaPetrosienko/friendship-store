fa84be443a6742dd6ca96c713e56db377b8b30c1
Gerasichev <gerasichev.serg@gmail.com>
2023-06-02 17:17:37 +0300

    Add AppAlert

--- frontend/src/layouts/Default.vue
+++ frontend/src/layouts/Default.vue
@@ -1,5 +1,14 @@
+<script setup lang="ts">
+import TheHeader from '@/components/header/TheHeader.vue';
+import AppAlert from '@/components/alert/AppAlert.vue';
+
+</script>
+
 <template>
-  <div>
+  <div class="default-layout">
+    <div class="container container_alert">
+      <AppAlert class="alert"/>
+    </div>
     <TheHeader />
     <main>
       <router-view />
@@ -7,11 +16,16 @@
   </div>
 </template>
 
-<script setup lang="ts">
-import TheHeader from '@/components/header/TheHeader.vue';
+<style lang="scss" scoped>
+.container_alert {
+  position: relative;
+}
 
-</script>
-
-<style scoped>
-
+.alert {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 60;
+  width: 100%;
+}
 </style>
--- frontend/src/layouts/Empty.vue
+++ frontend/src/layouts/Empty.vue
@@ -1,13 +1,29 @@
+<script setup lang="ts">
+import AppAlert from '@/components/alert/AppAlert.vue';
+
+</script>
+
 <template>
-  <div>
+  <div class="empty-layout">
+    <div class="container container_alert">
+      <AppAlert class="alert"/>
+    </div>
     <main>
       <router-view />
     </main>
   </div>
 </template>
 
-<script setup lang="ts">
+<style lang="scss" scoped>
+.container_alert {
+  position: relative;
+}
 
-</script>
-
-<style scoped></style>
+.alert {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 60;
+  width: 100%;
+}
+</style>
