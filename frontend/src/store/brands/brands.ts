784a3bfb5f052d70da69c6af4481c1d8514bad70
Gerasichev <gerasichev.serg@gmail.com>
2023-06-03 15:57:33 +0300

    Add Alert hadler

--- frontend/src/store/brands/brands.ts
+++ frontend/src/store/brands/brands.ts
@@ -4,17 +4,28 @@ import axios from 'axios';
 
 import { IBrand } from '@/store/brands/brands.interfaces';
 
+import { useCommonStore } from '@/store/common/common';
+
+const commonStore = useCommonStore();
+
 export const useBrandsStore = defineStore('brands', {
   state: () => ({
     brands: [] as IBrand[],
   }),
   actions: {
     async fetchBrands() {
+      commonStore.clearAlert();
+      commonStore.setLoading(true);
       try {
         const respone = await axios.get('http://127.0.0.1:8000/api/v1/brands/');
         this.brands = await respone.data;
+        commonStore.setLoading(false);
       } catch (error) {
-        console.error(error);
+        commonStore.setLoading(false);
+        commonStore.setAlertInfo({  
+          info: error.response.data.error,
+          status: 'danger'
+        });
         throw error;
       }
     },
