1163572af9ad3b050ba95528c171d17034ca94b0
Gerasichev <gerasichev.serg@gmail.com>
2023-06-03 15:54:34 +0300

    Add Alert handler

--- frontend/src/store/orders/orders.ts
+++ frontend/src/store/orders/orders.ts
@@ -4,16 +4,31 @@ import axios from 'axios';
 
 import { IOrder } from '@/store/orders/orders.interfaces';
 
+import { useCommonStore } from '@/store/common/common';
+
+const commonStore = useCommonStore();
+
 export const useOrdersStore = defineStore('orders', {
   state: () => ({
     order: {} as IOrder,
   }),
   actions: {
     async createOrder() {
+      commonStore.clearAlert();
+      commonStore.setLoading(true);
       try {
         const respone = await axios.post('http://127.0.0.1:8000/api/v1/order/', this.order);
+        commonStore.setLoading(false);
+        commonStore.setAlertInfo({
+          info: 'Заказ был успешно создан',
+          status: 'success'
+        });
       } catch (error) {
-        console.error(error);
+        commonStore.setLoading(false);
+        commonStore.setAlertInfo({  
+          info: error.response.data.error,
+          status: 'error'
+        });
         throw error;
       }
     },
