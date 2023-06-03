749e3a0c90ace626bbc26384930609532c3c4067
Gerasichev <gerasichev.serg@gmail.com>
2023-06-03 15:56:21 +0300

    Add Alert handler

--- frontend/src/store/news/news.ts
+++ frontend/src/store/news/news.ts
@@ -4,17 +4,28 @@ import axios from 'axios';
 
 import { INews } from '@/store/news/news.interfaces';
 
+import { useCommonStore } from '@/store/common/common';
+
+const commonStore = useCommonStore();
+
 export const useNewsStore = defineStore('news', {
   state: () => ({
     news: [] as INews[],
   }),
   actions: {
     async fetchNews() {
+      commonStore.clearAlert();
+      commonStore.setLoading(true);
       try {
         const respone = await axios.get('http://127.0.0.1:8000/api/v1/news/');
         this.news = await respone.data;
+        commonStore.setLoading(false);
       } catch (error) {
-        console.error(error);
+        commonStore.setLoading(false);
+        commonStore.setAlertInfo({  
+          info: error.response.data.error,
+          status: 'danger'
+        });
         throw error;
       }
     }
