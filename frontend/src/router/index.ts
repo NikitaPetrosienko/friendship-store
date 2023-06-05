ffb46f99141c58e6c3c026ddafbfe81fdab079c1
curtcobain2003 <petrosienko.nikita@mail.ru>
2023-06-05 21:46:47 +0300

    Delete ScrollBehavior

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -148,18 +148,6 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-  // scrollBehavior(to, from, savedPosition) {
-  //   if (to.hash) {
-  //     return {
-  //       el: to.hash,
-  //       behavior: 'smooth',
-  //     };
-  //   } else if (savedPosition) {
-  //     return savedPosition;
-  //   } else {
-  //     return { top: 0 };
-  //   }
-  // },
 })
 
 export default router
