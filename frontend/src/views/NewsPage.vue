bc278fba4389e32fe481b23cb0d174f7c6a9f62d
Gerasichev <gerasichev.serg@gmail.com>
2023-06-05 12:55:39 +0300

    Create NewsPage

--- frontend/src/views/NewsPage.vue
+++ frontend/src/views/NewsPage.vue
@@ -1,34 +1,50 @@
 <script setup lang="ts">
 import AppBreadcrumb from '@/components/breadcrumb/AppBreadcrumb.vue'
 
+import { ref } from 'vue';
+
+const breadcrumbs = ref([
+  {
+    id: 1,
+    title: 'Главная',
+    url: '/'
+  },
+  {
+    id: 2,
+    title: 'Архив новостей',
+    url: '/news/1'
+  },
+  {
+    id: 3,
+    title: 'NIKE SB Dunk Low Pro Laser Blue',
+    url: '/news/1'
+  },
+]);
+
 </script>
 
 <template>
   <div class="news-page">
     <div class="container">
-      <AppBreadcrumb />
-      <h1 class="news-page__title">NIKE SB Dunk Low Pro Laser Blue</h1>
-    </div>
+      <AppBreadcrumb :breadcrumbs="breadcrumbs"/>
 
-    <div class="news-page__body">
-      <div class="container">
-        <p>Команда скейтшопа ДРУЖБА приветствует тебя и предлагает окунуться в мир скейтбординга и всего, что его
-          окружает.</p>
-        <p>Открыв свои двери в 2005 году, мы по сей день продолжаем бороться за распространение скейтбординга по всей
-          планете, да что там — по всей России! За это время нашими клиентами были представители всех регионов России, а
-          также многих стран ближнего и дальнего зарубежья, соседних планет и галактик. Но при этом мы всегда оставались
-          небольшим локальным скейтшопом и уверены, что именно это позволяет нам наладить доверительные дружеские
-          отношения с нашими клиентами.
-        </p>
-        <p>Ведь скейтбординг — не про большие деньги, крупные сети, гигантские моллы и модные бутики. Для нас скейтбординг
-          — это фан, прикол и хорошее расслабленное настроение, а не серьезный олимпийский вид спорта с судьями и баллами.
-          Это катание на парковках со своими друзьями, самодельные скейтпарки, локальные контесты, теплые солнечные дни на
-          доске и долгие зимние вечера в ожидании нового сезона, и, конечно же, удовольствие от самого процесса, которое
-          может принести эта самая досточка со шкурочкой и колесиками.
-        </p>
+      <div class="news-page__grid">
+        <div>
+          <h1>NIKE SB Dunk Low Pro Laser Blue</h1>
+          <img src="../assets/img/news/img1.png" alt="news">
+          <p>
+            В 2020 году Nike SB провели довольно очевидную связь между своими любимыми Dunk и Air Max 90. Воспользовавшись растущей шумихой вокруг Данков, команда Nike SB привлекла дополнительное внимание к беговой обуви Air Max 90, которая в прошлом году праздновала свое 30-летие. Для тех, кто является поклонником Air Max, 2020 год стал невероятным годом для переизданий оригинальных цветов, но кто мог предположить, что они появятся на SB Dunk?
+          </p>
+        </div>
+        <div>
+          <p>Nike SB Dunk Low Pro “Laser Blue” — очередной релиз, вдохновленный моделью Nike Air Max 90.
+            После того, как в прошлом году была выпущена расцветка SB Данков, вдохновленная классической моделью Nike Air Max 90 “Infrared”, еще одна классическая расцветка Air Max 90 “Laser Blue” была использована в качестве вдохновения для очередного релиза Nike SB Dunk Low.
+          </p>
+          <img src="../assets/img/news/img1.png" alt="news">
+        </div>
       </div>
-    </div>
 
+    </div>
   </div>
 </template>
 
@@ -36,7 +52,16 @@ import AppBreadcrumb from '@/components/breadcrumb/AppBreadcrumb.vue'
 @import '@/assets/scss/_variables.scss';
 @import '@/assets/scss/_mixins.scss';
 
-.news-page__title {
+.news-page__grid {
+  display: grid;
+  grid-template-columns: 6fr 6fr;
+  grid-gap: 20px;
+  @include for-size(tablet) {
+    grid-template-columns: 12fr;
+  }
+}
+
+.news-page__grid h1 {
   @include font(48px, 400, 60px);
   color: $purple;
   margin: 8px 0 5px;
@@ -46,19 +71,20 @@ import AppBreadcrumb from '@/components/breadcrumb/AppBreadcrumb.vue'
   }
 }
 
-.news-page__body {
-  background-image: url(../assets/img/aboutus-bg1.png);
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  padding: 10px 0 10px;
+.news-page__grid div:last-child{
+  padding-top: 50px;
 }
 
-.news-page__body p {
-  @include font(36px, 500, 43px);
-  color: $black;
+.news-page__grid img {
+  width: 100%;
+}
 
-  @include for-size(tablet) {
-    @include font(16px, 400, 1.2);
-  }
-}</style>
+// .news-page__body p {
+//   @include font(36px, 500, 43px);
+//   color: $black;
+
+//   @include for-size(tablet) {
+//     @include font(16px, 400, 1.2);
+//   }
+// }
+</style>
